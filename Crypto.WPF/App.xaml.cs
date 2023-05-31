0d19bb3e5364a3dfe08dc25243f815c9d1e896c8
51xel <20rud15@gmail.com>
2023-05-31 21:50:27 +0300

    Added TopCryptoView and elements in it

--- Crypto.WPF/App.xaml.cs
+++ Crypto.WPF/App.xaml.cs
@@ -7,9 +7,12 @@ using System.Threading.Tasks;
 using System.Windows;
 
 namespace Crypto.WPF {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application {
+        protected override void OnStartup(StartupEventArgs e) {
+            MainWindow = new MainWindow();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
---
+++ Crypto.WPF/Views/TopCryptoView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Crypto.WPF.Views {
+    /// <summary>
+    /// Interaction logic for TopCryptoView.xaml
+    /// </summary>
+    public partial class TopCryptoView : UserControl {
+        private class Pl {
+            public string Name { get; set; }
+            public int Price { get; set; }
+        }
+        public TopCryptoView() {
+            InitializeComponent();
+
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+            test.List.Items.Add(new Pl {Name = "Bitcoin", Price = 2165465 });
+        }
+    }
+}
